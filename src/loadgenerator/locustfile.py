132203994724c2059691dbc1c07bb952414719ef
marciokugler <marciokugler@gmail.com>
2024-02-28 21:13:41 -0600

    reverting locus, updating payment service to add resource detector

--- src/loadgenerator/locustfile.py
+++ src/loadgenerator/locustfile.py
@@ -69,60 +69,51 @@ people_file = open('people.json')
 people = json.load(people_file)
 
 
+class WebsiteUser(HttpUser):
+    wait_time = between(10, 10)
 
+    @task(1)
+    def index(self):
+        self.client.get("/")
 
-products = [
-    '0PUK6V6EV0',
-    '1YMWWN1N4O',
-    '2ZYFJ3GM2N',
-    '66VCHSJNUP',
-    '6E92ZMYYFZ',
-    '9SIQT8TOJO',
-    'L9ECAV7KIM',
-    'LS4PSXUNUM',
-    'OLJCESPC7Z']
+    @task(2)
+    def browse_product(self):
+        self.client.get("/product/" + random.choice(products))
 
-def index(l):
-    l.client.get("/")
+    @task(3)
+   
+    def add_to_cart(self):
+        product = random.choice(products)
+        self.client.get("/product/" + product)
+        self.client.post("/cart", {
+            'product_id': product,
+            'quantity': random.choice([1,2,3,4,5,10])})
+    
+    @task(4)
+    def view_cart(self):
+        self.client.get("/cart")
+        
+    @task(5)
+    def checkout(self):
+        self.add_to_cart(self)
+        self.client.post("/cart/checkout", {
+            'email': 'locust-user@example.com',
+            'street_address': '1600 Amphitheatre Parkway',
+            'zip_code': '94043',
+            'city': 'Mountain View',
+            'state': 'CA',
+            'country': 'United States',
+            'credit_card_number': '4432-8015-6152-0454',
+            'credit_card_expiration_month': '1',
+            'credit_card_expiration_year': '2039',
+            'credit_card_cvv': '672',
+        })
 
-def setCurrency(l):
-    currencies = ['EUR', 'USD', 'JPY', 'CAD']
-    l.client.post("/setCurrency",
-        {'currency_code': random.choice(currencies)})
-
-def browseProduct(l):
-    l.client.get("/product/" + random.choice(products))
-
-def viewCart(l):
-    l.client.get("/cart")
-
-def addToCart(l):
-    product = random.choice(products)
-    l.client.get("/product/" + product)
-    l.client.post("/cart", {
-        'product_id': product,
-        'quantity': random.choice([1,2,3,4,5,10])})
-
-def checkout(l):
-    addToCart(l)
-    l.client.post("/cart/checkout", {
-        'email': 'someone@example.com',
-        'street_address': '1600 Amphitheatre Parkway',
-        'zip_code': '94043',
-        'city': 'Mountain View',
-        'state': 'CA',
-        'country': 'United States',
-        'credit_card_number': '4432-8015-6152-0454',
-        'credit_card_expiration_month': '1',
-        'credit_card_expiration_year': '2039',
-        'credit_card_cvv': '672',
-    })
-
-
-def on_start(l):
-    ctx = baggage.set_baggage("synthetic_request", "true")
-    context.attach(ctx)
-    l.index()
+    @task(6)
+    def on_start(self):
+        ctx = baggage.set_baggage("synthetic_request", "true")
+        context.attach(ctx)
+        self.index()
 
 
 browser_traffic_enabled = os.environ.get("LOCUST_BROWSER_TRAFFIC_ENABLED", "").lower() in ("true", "yes", "on")
@@ -130,7 +121,7 @@ browser_traffic_enabled = os.environ.get("LOCUST_BROWSER_TRAFFIC_ENABLED", "").l
 if browser_traffic_enabled:
     class WebsiteBrowserUser(PlaywrightUser):
         headless = True  # to use a headless browser, without a GUI
-
+        
         @task
         @pw
         async def open_cart_page_and_change_currency(self, page: PageWithRetry):
@@ -155,16 +146,16 @@ if browser_traffic_enabled:
                 #Go to root website
                 await page.route('**/*', add_baggage_header)
                 await page.goto('/', add_baggage_header, wait_until="domcontentloaded")
-                time.sleep(5)
+                
                 #Click on the Sunglasses category
                 await page.click('p:has-text("Sunglasses")', wait_until="domcontentloaded")
-                time.sleep(5)
+                
                 #Add to Cart
                 await page.click('button:has-text("Add To Cart")', wait_until="domcontentloaded")
-                time.sleep(5)
+                
                 #Place order
                 await page.click('button:has-text("Place Order")', wait_until="domcontentloaded")
-                time.sleep(5)
+                
                 await page.wait_for_timeout(2000)  # giving the browser time to export the traces
             except:
                 pass
@@ -176,20 +167,3 @@ async def add_baggage_header(route: Route, request: Request):
         'baggage': 'synthetic_request=true'
     }
     await route.continue_(headers=headers)
-
-
-class UserBehavior(TaskSet):
-
-    def on_start(self):
-        index(self)
-
-    tasks = {index: 1,
-        setCurrency: 2,
-        browseProduct: 10,
-        addToCart: 2,
-        viewCart: 3,
-        checkout: 1}
-
-class WebsiteUser(HttpUser):
-    tasks = [UserBehavior]
-    wait_time = between(1, 10)
--- src/paymentservice/tracing.js
+++ src/paymentservice/tracing.js
@@ -1,10 +1,27 @@
-const opentelemetry = require("@opentelemetry/sdk-node");
-const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
-const { OTLPTraceExporter } =  require('@opentelemetry/exporter-trace-otlp-grpc');
+const opentelemetry = require('@opentelemetry/sdk-node');
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc');
+const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-grpc');
+const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
+const { awsEc2Detector, awsEksDetector } = require('@opentelemetry/resource-detector-aws');
+const { containerDetector } = require('@opentelemetry/resource-detector-container');
+const { envDetector, hostDetector, osDetector, processDetector } = require('@opentelemetry/resources');
 
 const sdk = new opentelemetry.NodeSDK({
-  traceExporter: new OTLPTraceExporter(),
-  instrumentations: [getNodeAutoInstrumentations()]
+    traceExporter: new OTLPTraceExporter(),
+    instrumentations: [getNodeAutoInstrumentations()],
+    metricReader: new PeriodicExportingMetricReader({
+        exporter: new OTLPMetricExporter(),
+    }),
+    resourceDetectors: [
+        containerDetector,
+        envDetector,
+        hostDetector,
+        osDetector,
+        processDetector,
+        awsEksDetector,
+        awsEc2Detector,
+    ],
 });
 
-sdk.start()
+sdk.start();
